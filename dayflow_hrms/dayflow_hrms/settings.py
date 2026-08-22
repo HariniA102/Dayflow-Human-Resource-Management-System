3ab750d0e34cb6dd6f52ed9638099bdcd60e1140
prema0207 <premau2006@gmail.com>
2026-08-22 13:32:13 +0530

    Add new applications to Django settings
    prema

--- dayflow_hrms/dayflow_hrms/settings.py
+++ dayflow_hrms/dayflow_hrms/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'employees',
+    'attendance',
+    'leaves',
+    'payroll',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
